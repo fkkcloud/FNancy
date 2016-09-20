0f74edc6ecf5265f5f3209c172d8e25e8c63e179
fkkcloud <fkkcloud@gmail.com>
2016-09-20 14:59:52 -0700

    Audio system setup, Level loading system setup, main menu, splash, game, option scenes, sounds

---
+++ Assets/Script/Manager/LevelManager.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+
+	public float autoLoadNextLevelDuration;
+	public bool autoLoadEnable;
+
+	void Start(){
+		if (autoLoadEnable) {
+			// Splash Screen -> Main Menu
+			Invoke ("LoadNextLevel", autoLoadNextLevelDuration);
+		}
+	}
+
+	public void LoadLevel(string name){
+		Debug.Log ("New Level load requested: " + name);
+		Application.LoadLevel (name);
+	}
+
+	public void QuitRequest(){
+		Debug.Log ("Quit requested");
+		Application.Quit ();
+	}
+
+	public void LoadNextLevel(){
+		Application.LoadLevel (Application.loadedLevel + 1);
+	}
+
+	public void LoadMainMenu(){
+		Application.LoadLevel ("01_000_MainMenu");
+	}
+
+}
+
