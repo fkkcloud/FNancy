0f1503449fb108baa8cab13fab532e663c30ba91
fkkcloud <fkkcloud@gmail.com>
2016-09-21 12:40:15 -0700

    animator init setup, JSON loader

---
+++ Assets/Script/Data/Stage.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+public class Stage
+{
+	public int level = 1; // current level
+	public float time = 1.0f; // json controlled for balance
+	public float reward = 0.0f;
+}
---
+++ Assets/Script/StaticUtils.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class StaticUtils : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	// create stage objects from json
+	public static Stage[] CreateStages(TextAsset StageJson){
+		if (StageJson){
+			string[] data = StageJson.text.Split ('$');
+
+			int count = data.Length;
+			Stage[] stages = new Stage[count];
+
+			for (int i = 0; i < data.Length; i++) {
+				string json = data [i];
+				Stage stage = JsonUtility.FromJson<Stage> (json);	
+				stages [i] = stage;
+			}
+			return stages;
+		}
+		return null;
+	}
+}
