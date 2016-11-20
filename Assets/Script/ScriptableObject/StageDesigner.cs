f9b87ab97e42282b461b664bcb7d9cd8ecd7c232
fkkcloud <fkkcloud@gmail.com>
2016-11-19 18:34:36 -0800

    setting up better flow

--- Assets/Script/ScriptableObject/StageDesigner.cs
+++ Assets/Script/ScriptableObject/StageDesigner.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[CreateAssetMenu(fileName = "Data", menuName = "GameDesign/StageDesigner", order = 1)]
+[CreateAssetMenu(fileName = "StageDesigner", menuName = "GameDesign/StageDesigner", order = 1)]
 public class StageDesigner : ScriptableObject
 {
 	public StageData[] stageDatas;
